da209d4b6339bc8bd28d71da282cf514bbb42904
Harsh Khare <harsh221094@gmail.com>
2019-06-01 20:48:34 +0530

    New database structure integration
    New

--- app.js
+++ app.js
@@ -1,58 +1,80 @@
 var firebaseConfig = {
   apiKey: "AIzaSyBA1tSl76muFV6mE-H6BbG8a3a15StAv4A",
+
   authDomain: "heredata-6181c.firebaseapp.com",
+
   databaseURL: "https://heredata-6181c.firebaseio.com",
+
   projectId: "heredata-6181c",
+
   storageBucket: "heredata-6181c.appspot.com",
+
   messagingSenderId: "936389003107",
+
   appId: "1:936389003107:web:35a65f5ea9469ab6"
 };
+
 // Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
+
 var firestore = firebase.firestore();
 
-// sets project name value to null
-document.getElementById("projectname").selectedIndex = -1;
-// sets project name properly 
-var pname= document.getElementById('projectname').value;
-const docRef= firestore.doc("production/data/"+pname);
-
-
-function calldata(id){
-return document.getElementById(id).value;
+function calldata(id) {
+  return document.getElementById(id).value;
 }
 
-document.getElementById('mainform').addEventListener('submit', storethedata);
+document.getElementById("mainform").addEventListener("submit", storethedata);
 
+function storethedata(e) {
+  e.preventDefault();
 
-function storethedata(e){
-e.preventDefault();
+  const name = calldata("name");
 
-const name= calldata('name');
-const projectname= calldata('projectname');
-const keyhighlights= calldata('keyhighlights');
-const achievements= calldata('achievements');
-const concerns= calldata('concerns');
-const othrconcerns= calldata('othrconcerns');
-const upevents= calldata('upevents');
-const datedata= calldata('datedata');
+  const projectname = calldata("projectname");
 
-console.log("Storing data for " + name +" now!");
-  docRef.set({
-    name: name,
-    datedata: datedata,
-    projectname: projectname,
-    keyhighlights: keyhighlights,
-    achievements: achievements,
-    concerns: concerns,
-    othrconcerns: othrconcerns,
-    upevents: upevents
-}).then(function(){
-    console.log("Data saved!");
-    document.getElementById('successbar').innerHTML="SUBMIT SUCCESSFUL!";
-}).catch(function (error){
-    console.log("Got an error!", error);
-    document.getElementById('failbar').innerHTML="SUBMIT UNSUCCESSFUL!";
-});
-}; 
+  const keyhighlights = calldata("keyhighlights");
 
+  const achievements = calldata("achievements");
+
+  const concerns = calldata("concerns");
+
+  const othrconcerns = calldata("othrconcerns");
+
+  const upevents = calldata("upevents");
+
+  const datedata = calldata("datedata");
+
+  const docRef = firestore.doc("NMP/" + projectname);
+
+  console.log("Storing data for " + name + " now!");
+
+  docRef
+    .set({
+      name: name,
+
+      datedata: datedata,
+
+      projectname: projectname,
+
+      keyhighlights: keyhighlights,
+
+      achievements: achievements,
+
+      concerns: concerns,
+
+      othrconcerns: othrconcerns,
+
+      upevents: upevents
+    })
+    .then(function() {
+      console.log("Data saved!");
+
+      document.getElementById("successbar").innerHTML = "SUBMIT SUCCESSFUL!";
+    })
+    .catch(function(error) {
+      console.log("Got an error!", error);
+
+      document.getElementById("failbar").innerHTML = "SUBMIT UNSUCCESSFUL!";
+    });
+}
